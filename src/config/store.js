4184aea690f1b466382ac2ef95c5ca9ef13f0e53
Martyna <martyna.kolakowska94@gmail.com>
2019-01-05 17:31:32 +0100

    store update

--- src/config/store.js
+++ src/config/store.js
@@ -1,22 +1,36 @@
 
-import {createStore, combineReducers,applyMiddleware } from 'redux';
+import {createStore, combineReducers,applyMiddleware,compose } from 'redux';
 import cartReducer from '../Components/cart/reducer'
 import productsReducer from '../Components/product/productsReducer';
 import thunk from 'redux-thunk';
-// import reduxFormReducer from '../Components/form/reducer'
-import { reducer as reduxFormReducer} from 'redux-form/immutable';
+import reduxFormSampleReducer from '../Components/form/reducer'
+// import { reducer as reduxFormReducer} from 'redux-form/immutable';
 
 const rootReducer = combineReducers ({
     cart: cartReducer,
-    form: reduxFormReducer,
+    form: reduxFormSampleReducer,
     products: productsReducer
 
 })
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk)
-)
+// const store = createStore(
+//     rootReducer,
+//     applyMiddleware(thunk),
+//      /* preloadedState, */
+// +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+const store = createStore(rootReducer, enhancer);
 
 export default store
