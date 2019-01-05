d1d0cd9b41133e4c1b95933239621bfc7df3acdd
Martyna <martyna.kolakowska94@gmail.com>
2019-01-05 17:20:05 +0100

    merge fix

--- src/Components/form/selectors.js
+++ src/Components/form/selectors.js
@@ -1,3 +1,3 @@
-export const formHasErrorsSelector = state => console.log(state);
+export const formHasErrorsSelector = state => state.getIn(['form', 'formHasErrors']);
 export const getFormErrorsSelector = state => state.getIn(['form', 'orderForm', 'submitErrors']);
 
